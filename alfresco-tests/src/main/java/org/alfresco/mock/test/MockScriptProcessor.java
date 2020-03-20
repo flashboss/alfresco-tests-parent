eccf75c66157b14aa715e6dd3d750c9614d15bab
Luca Stancapiano <luca.stancapiano@vige.it>
2020-03-20 20:19:50 +0100

    activiti tests

---
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/MockScriptProcessor.java
@@ -0,0 +1,9 @@
+package org.alfresco.mock.test;
+
+import java.io.Serializable;
+
+import org.alfresco.repo.jscript.RhinoScriptProcessor;
+
+public class MockScriptProcessor extends RhinoScriptProcessor implements Serializable {
+
+}
