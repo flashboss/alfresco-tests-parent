16a3cd2c77153ad615bc12716af2917c8389d551
Luca Stancapiano <luca.stancapiano@vige.it>
2020-07-14 13:28:58 +0200

    new action service

--- alfresco-tests/src/main/java/org/alfresco/mock/test/ws/AbstractWSForm.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/ws/AbstractWSForm.java
@@ -1,5 +1,8 @@
 package org.alfresco.mock.test.ws;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.alfresco.mock.test.AbstractForm;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,8 @@ public abstract class AbstractWSForm extends AbstractForm {
 	
 	@Autowired
 	private Description description;
+	
+	protected DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmm");
 
 	protected abstract DeclarativeWebScript getDeclarativeWebScript();
 
