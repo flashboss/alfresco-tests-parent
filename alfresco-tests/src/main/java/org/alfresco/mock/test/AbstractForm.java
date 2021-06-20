4be50ce31f1849578bd70c04ce3b1477ea73ba3a
Luca Stancapiano <lucastancapiano@192.168.1.39>
2021-06-20 20:52:56 +0200

    authority service

--- alfresco-tests/src/main/java/org/alfresco/mock/test/AbstractForm.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/AbstractForm.java
@@ -62,8 +62,10 @@ public abstract class AbstractForm {
 		NodeRef root = insertFolder(new NodeRef(new StoreRef("", ""), ""), ".");
 		workspace = insertFolder(root, StoreRef.PROTOCOL_WORKSPACE);
 		NodeRef companyHome = insertFolder(workspace, NamespaceService.APP_MODEL_PREFIX, "company_home");
+		NodeRef system = insertFolder(workspace, NamespaceService.SYSTEM_MODEL_PREFIX, "system");
 		archive = insertFolder(root, StoreRef.PROTOCOL_ARCHIVE);
 		sites = insertFolder(companyHome, SiteModel.SITE_MODEL_PREFIX, SiteModel.TYPE_SITES.getLocalName());
+		insertFolder(system, SiteModel.SITE_MODEL_PREFIX, "authorities");
 	}
 
 	protected NodeRef insertFolder(NodeRef parent, String name) {
