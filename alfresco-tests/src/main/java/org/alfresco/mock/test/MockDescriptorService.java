4147627cd8fd9b87b526a6494edd75f60977e9cd
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2024-01-23 11:39:40 +0100

    transfer service

---
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/MockDescriptorService.java
@@ -0,0 +1,73 @@
+package org.alfresco.mock.test;
+
+import java.io.InputStream;
+
+import org.alfresco.service.descriptor.Descriptor;
+import org.alfresco.service.descriptor.DescriptorService;
+import org.alfresco.service.license.LicenseDescriptor;
+import org.alfresco.service.license.LicenseService.LicenseChangeHandler;
+import org.springframework.beans.factory.InitializingBean;
+
+public class MockDescriptorService implements DescriptorService, InitializingBean, LicenseChangeHandler {
+
+	@Override
+	public void onLicenseChange(LicenseDescriptor licenseDescriptor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLicenseFail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Descriptor getServerDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Descriptor getCurrentRepositoryDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Descriptor getInstalledRepositoryDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LicenseDescriptor getLicenseDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadLicense() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadLicense(InputStream licenseStream) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isBootstrapped() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
