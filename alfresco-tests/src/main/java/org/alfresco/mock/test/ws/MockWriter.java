133a659a8a371999272ecd489653d376c4f0b30a
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2024-03-31 15:05:29 +0200

    model for webscript, company home resource and versioning history date

---
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/ws/MockWriter.java
@@ -0,0 +1,31 @@
+package org.alfresco.mock.test.ws;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class MockWriter extends Writer {
+
+	private Map<String, Object> model;
+
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+	}
+
+	@Override
+	public void flush() throws IOException {
+	}
+
+	@Override
+	public void close() throws IOException {
+	}
+
+	public Map<String, Object> getModel() {
+		return model;
+	}
+
+	public void setModel(Map<String, Object> model) {
+		this.model = model;
+	}
+
+}
