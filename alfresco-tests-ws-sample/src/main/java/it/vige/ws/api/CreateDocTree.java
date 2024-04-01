80568a711583b9066b1b447e1d06462f291f14f3
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2024-04-01 13:04:10 +0200

    model for webscript, company home resource and versioning history date

--- alfresco-tests-ws-sample/src/main/java/it/vige/ws/api/CreateDocTree.java
+++ alfresco-tests-ws-sample/src/main/java/it/vige/ws/api/CreateDocTree.java
@@ -17,7 +17,7 @@ import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.namespace.QName;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
