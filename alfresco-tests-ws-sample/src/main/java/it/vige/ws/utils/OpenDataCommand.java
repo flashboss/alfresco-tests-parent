8f172948fe189d879d50c34e56f90c1f42a3585a
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2024-04-29 14:30:25 +0200

    ws test

--- alfresco-tests-ws-sample/src/main/java/it/vige/ws/utils/OpenDataCommand.java
+++ alfresco-tests-ws-sample/src/main/java/it/vige/ws/utils/OpenDataCommand.java
@@ -1,19 +1,7 @@
 package it.vige.ws.utils;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.alfresco.model.ContentModel;
-import org.alfresco.service.cmr.repository.ChildAssociationRef;
-import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.repository.XPathException;
-import org.alfresco.service.cmr.search.SearchService;
-import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -36,29 +24,6 @@ public class OpenDataCommand {
 	public final String DELETE_ATTO = "DeleteATTO";
 
 	private NodeService nodeService;
-	private String listSeparator;
-	private String linkVotoFinale;
-
-	private String linkAtto;
-	private SearchService searchService;
-	private NamespaceService namespaceService;
-	private AttoUtil attoUtil;
-
-	public void setLinkVotoFinale(String linkVotoFinale) {
-		this.linkVotoFinale = linkVotoFinale;
-	}
-
-	public void setAttoUtil(AttoUtil attoUtil) {
-		this.attoUtil = attoUtil;
-	}
-
-	public void setNamespaceService(NamespaceService namespaceService) {
-		this.namespaceService = namespaceService;
-	}
-
-	public void setSearchService(SearchService searchService) {
-		this.searchService = searchService;
-	}
 
 	public OpenDataCommand() {
 		super();
@@ -154,63 +119,6 @@ public class OpenDataCommand {
 
 	public void setNodeService(NodeService nodeService) {
 		this.nodeService = nodeService;
-	}
-
-	public void setListSeparator(String listSeparator) {
-		this.listSeparator = listSeparator;
-	}
-
-	public void setLinkAtto(String linkAtto) {
-		this.linkAtto = linkAtto;
-	}
-
-	/**
-	 * Ottiene il link all'atto
-	 * 
-	 * @param nodeRef
-	 *                atto dal quale ottenere il link
-	 * @return String link per l'atto
-	 */
-	public String getLinkAtto(NodeRef nodeRef) {
-		try {
-			MessageFormat mf = new MessageFormat(this.linkAtto);
-			String parameterFormatting[] = new String[] {
-					(String) nodeService.getProperty(nodeRef, ContentModel.PROP_NODE_UUID) };
-			return mf.format(parameterFormatting);
-		} catch (InvalidNodeRefException e) {
-			logger.error("Eccezione nella formattazione", e);
-			return null;
-		}
-	}
-
-	/**
-	 * Ottiene il tipo di iniziativa di un nodo: Consiliare, Popolare, Ufficio di
-	 * Presidenza, Consiglio delle Autonomie locali, Presidente della Giunta,
-	 * Giunta, Commissioni, Altra Iniziativa
-	 * 
-	 * @param nodeRef
-	 *                Nodo dal quale ottenere l'iniziativa
-	 * @return String con l'iniziativa dell'atto
-	 */
-	public String getTipoIniziativa(NodeRef nodeRef) {
-		String tipoIniziativa = (String) nodeService.getProperty(nodeRef, AttoUtil.PROP_TIPO_INIZIATIVA_QNAME);
-		if (tipoIniziativa != null) {
-			if ("01_ATTO DI INIZIATIVA CONSILIARE".equals(tipoIniziativa)) {
-
-				return "Consiliare";
-
-			} else if ("03_ATTO DI INIZIATIVA POPOLARE".equals(tipoIniziativa)) {
-
-				return "Popolare";
-
-			} else if ("05_ATTO DI INIZIATIVA UFFICIO PRESIDENZA".equals(tipoIniziativa)) {
-
-				return "Ufficio di Presidenza";
-
-			}
-		}
-		return null;
-
 	}
 
 	public String buildSoapEnvelope(String nomeChiamata, String odAtto, String privateToken, String ambiente,
