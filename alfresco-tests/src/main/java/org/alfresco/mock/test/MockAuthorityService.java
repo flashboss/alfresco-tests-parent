dc5ab488bebc2ba6c2a4027d6475c9bfab987187
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-08-17 15:32:21 +0200

    Align mock space lookup with Alfresco NodeService and AuthorityService.
    Use production cm:name values, filter child assocs by QName, and create groups under sys:system/sys:authorities.

--- alfresco-tests/src/main/java/org/alfresco/mock/test/MockAuthorityService.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/MockAuthorityService.java
@@ -2,8 +2,6 @@ package org.alfresco.mock.test;
 
 import static org.alfresco.model.ContentModel.ASSOC_CONTAINS;
 import static org.alfresco.model.ContentModel.TYPE_CONTENT;
-import static org.alfresco.model.ContentModel.TYPE_FOLDER;
-import static org.alfresco.service.cmr.repository.StoreRef.PROTOCOL_WORKSPACE;
 import static org.alfresco.service.cmr.repository.StoreRef.STORE_REF_WORKSPACE_SPACESSTORE;
 import static org.alfresco.service.namespace.NamespaceService.CONTENT_MODEL_1_0_URI;
 import static org.alfresco.service.namespace.QName.createQName;
@@ -26,6 +24,8 @@ import org.alfresco.service.cmr.security.AuthorityService;
 import org.alfresco.service.cmr.security.AuthorityType;
 import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.alfresco.model.ContentModel.ASSOC_CHILDREN;
+import org.alfresco.mock.NodeUtils;
 
 public class MockAuthorityService implements AuthorityService, Serializable {
 
@@ -117,9 +117,9 @@ public class MockAuthorityService implements AuthorityService, Serializable {
 	public String createAuthority(AuthorityType type, String shortName, String authorityDisplayName,
 			Set<String> authorityZones) {
 		String name = getName(null, shortName);
-		NodeRef root = nodeService.getRootNode(new StoreRef(PROTOCOL_WORKSPACE, STORE_REF_WORKSPACE_SPACESSTORE.getIdentifier()));
-		NodeRef system = nodeService.getChildByName(root, TYPE_FOLDER, "system");
-		NodeRef authorities = nodeService.getChildByName(system, TYPE_FOLDER, "authorities");
+		NodeRef root = nodeService.getRootNode(STORE_REF_WORKSPACE_SPACESSTORE);
+		NodeRef system = nodeService.getChildByName(root, ASSOC_CHILDREN, NodeUtils.toAlfrescoCmName("system"));
+		NodeRef authorities = nodeService.getChildByName(system, ASSOC_CONTAINS, NodeUtils.toAlfrescoCmName("authorities"));
 		QName assocQName = createQName(CONTENT_MODEL_1_0_URI, name);
 		NodeRef node = nodeService.createNode(authorities, ASSOC_CONTAINS, assocQName, TYPE_CONTENT).getChildRef();
 		authorityNodes.put(name, node);
--- alfresco-tests/src/main/java/org/alfresco/mock/test/activiti/AbstractActivitiForm.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/activiti/AbstractActivitiForm.java
@@ -102,7 +102,7 @@ public abstract class AbstractActivitiForm extends ResourceActivitiTestCase {
 		archive = insertFolder(root, StoreRef.PROTOCOL_ARCHIVE);
 		sites = insertFolder(companyHome, SiteModel.SITE_MODEL_PREFIX, SiteModel.TYPE_SITES.getLocalName());
 		shared = insertFolder(companyHome, NamespaceService.APP_MODEL_PREFIX, "shared");
-		insertFolder(system, SiteModel.SITE_MODEL_PREFIX, "authorities");
+		insertFolder(system, NamespaceService.SYSTEM_MODEL_PREFIX, "authorities");
 		NodeRef workflow = insertFolder(spacesStore, "workflow");
 		NodeRef packages = insertFolder(workflow, "packages");
 		NodeRef bpmPackageFolder = insertFolder(packages, "pkg_919f220e-870a-4c56-ba11-5030ee5325f0");
@@ -142,13 +142,11 @@ public abstract class AbstractActivitiForm extends ResourceActivitiTestCase {
 	}
 
 	protected NodeRef insertFolder(NodeRef parent, String prefix, String localName) {
-		ServiceRegistry serviceRegistry = ((ActivitiProcessEngineConfiguration) processEngineConfiguration)
-				.getServiceRegistry();
-		FileFolderService fileFolderService = serviceRegistry.getFileFolderService();
-		NamespaceService namespaceService = serviceRegistry.getNamespaceService();
-		QName qname = QName.createQName(prefix, localName, namespaceService);
-		return fileFolderService.create(parent, qname.getPrefixString(), ContentModel.TYPE_FOLDER).getNodeRef();
-	}
+    ServiceRegistry serviceRegistry = ((ActivitiProcessEngineConfiguration) processEngineConfiguration)
+        .getServiceRegistry();
+    return NodeUtils.insertFolder(parent, prefix, localName, serviceRegistry.getNodeService(),
+        serviceRegistry.getNamespaceService());
+  }
 
 	protected NodeRef insertDocument(NodeRef parent, String name, String text, Map<QName, Serializable> properties) {
 		ActivitiProcessEngineConfiguration activitiProcessEngineConfiguration = (ActivitiProcessEngineConfiguration) processEngineConfiguration;
