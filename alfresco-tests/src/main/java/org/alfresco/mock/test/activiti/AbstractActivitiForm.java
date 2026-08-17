b7b5c17c7139ec091aa8e43808a084eb30f00631
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-08-17 15:32:29 +0200

    Align mock space lookup with Alfresco NodeService and AuthorityService.
    Use production cm:name values, filter child assocs by QName, and create groups under sys:system/sys:authorities.

--- alfresco-tests/src/main/java/org/alfresco/mock/test/activiti/AbstractActivitiForm.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/activiti/AbstractActivitiForm.java
@@ -137,7 +137,7 @@ public abstract class AbstractActivitiForm extends ResourceActivitiTestCase {
     sites =
         insertFolder(companyHome, SiteModel.SITE_MODEL_PREFIX, SiteModel.TYPE_SITES.getLocalName());
     shared = insertFolder(companyHome, NamespaceService.APP_MODEL_PREFIX, "shared");
-    insertFolder(system, SiteModel.SITE_MODEL_PREFIX, "authorities");
+    insertFolder(system, NamespaceService.SYSTEM_MODEL_PREFIX, "authorities");
     NodeRef workflow = insertFolder(spacesStore, "workflow");
     NodeRef packages = insertFolder(workflow, "packages");
     NodeRef bpmPackageFolder = insertFolder(packages, "pkg_919f220e-870a-4c56-ba11-5030ee5325f0");
@@ -196,16 +196,10 @@ public abstract class AbstractActivitiForm extends ResourceActivitiTestCase {
    * @return the node ref
    */
   protected NodeRef insertFolder(NodeRef parent, String prefix, String localName) {
-    ServiceRegistry serviceRegistry =
-        ((ActivitiProcessEngineConfiguration) processEngineConfiguration).getServiceRegistry();
-    FileFolderService fileFolderService = serviceRegistry.getFileFolderService();
-    NamespaceService namespaceService = serviceRegistry.getNamespaceService();
-
-    /** The qname. */
-    QName qname = QName.createQName(prefix, localName, namespaceService);
-    return fileFolderService
-        .create(parent, qname.getPrefixString(), ContentModel.TYPE_FOLDER)
-        .getNodeRef();
+    ServiceRegistry serviceRegistry = ((ActivitiProcessEngineConfiguration) processEngineConfiguration)
+        .getServiceRegistry();
+    return NodeUtils.insertFolder(parent, prefix, localName, serviceRegistry.getNodeService(),
+        serviceRegistry.getNamespaceService());
   }
 
   /**
