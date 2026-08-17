c61471110b4faa7ea05b46e5a74f2a6a989323a5
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-08-17 15:57:24 +0200

    Add getPathAsString helper used by child association lookup.

--- alfresco-tests/src/main/java/org/alfresco/mock/test/MockNodeService.java
+++ alfresco-tests/src/main/java/org/alfresco/mock/test/MockNodeService.java
@@ -447,6 +447,13 @@ public class MockNodeService implements NodeService {
 			return null;
 	}
 
+	public String getPathAsString(NodeRef nodeRef) throws InvalidNodeRefException {
+		Path path = getPath(nodeRef);
+		if (path != null)
+			return path.toString().replace("\", "/");
+		return null;
+	}
+
 	@Override
 	public List<Path> getPaths(NodeRef nodeRef, boolean primaryOnly) throws InvalidNodeRefException {
 		// TODO Auto-generated method stub
